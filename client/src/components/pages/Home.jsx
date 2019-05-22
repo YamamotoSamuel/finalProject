faca3003eba5e047cf2706f54a33b79b321e74d3
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-05-22 15:11:57 -0400

    modified home jsx

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
 export default class Home extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //   }
-  // }
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: ''
+    }
+  }
+  componentDidMount(){
+     
+  }
+  getActivity = () => {
+    Axios.get('https://www.boredapi.com/api/activity')
+    .then(theTask => {
+      console.log(theTask,123412342134,Date.now())
+      this.setState ({
+        task: theTask.data.activity
+      })
+    })
+  }
   render() {                
     return (
       <div className="Home">
         <h2>Home</h2>
-        <p>This is a sample project with the MERN stack</p>
+        <p>This is my final project with the MERN stack</p>
+        {this.state.task}<br />
+        <button className="give-task" onClick={this.getActivity}>Get a Random Activity</button>
       </div>
     );
   }

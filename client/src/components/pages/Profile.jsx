d3bd8844063a8d15a648d52726a3d54d8ed4fea7
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-05-23 17:10:18 -0400

    Modified file to display all tasks that ALL users have created, now have to update code so that only tasks displayed are from the logged in user

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import ShowTasks from './ShowTasks'
 
 export default class Profile extends Component {
   render() {
     return (
       <div>
-        Profile page
+        <div>
+        Profile page<br/>
+        {this.props.user.username}
+        {this.props.user.name}
+        </div>
+        <div className="show-user-tasks">
+          <ShowTasks />
+        </div>
       </div>
     )
   }

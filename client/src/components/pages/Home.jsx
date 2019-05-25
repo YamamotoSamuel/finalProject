776d599db236771c5152bd4e4ca6e49ed6d6ee9d
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-05-24 23:38:00 -0400

    component now shows all necessary info for randomly generated activities from bored api

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,32 +1,73 @@
-import React, { Component } from 'react';
-import Axios from 'axios';
-
+import React, { Component } from "react";
+import axios from "axios";
+import AddTask from "./AddTask";
+import ShowTasks from "./ShowTasks";
+axios.defaults.withCredentials = true;
 export default class Home extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      task: ''
-    }
+      task: {},
+      tasks: [],
+      refreshShow: false
+      // checked: false
+    };
   }
-  componentDidMount(){
-     
-  }
+
+  handleCheckboxChange = event =>
+    this.setState({ checked: event.target.checked });
+  componentDidMount() {}
   getActivity = () => {
-    Axios.get('https://www.boredapi.com/api/activity')
-    .then(theTask => {
-      console.log(theTask,123412342134,Date.now())
-      this.setState ({
-        task: theTask.data.activity
-      })
-    })
+    console.log("getting activity");
+    axios
+      .get("https://www.boredapi.com/api/activity", { withCredentials: false })
+      .then(theTask => {
+        console.log("the task is", theTask);
+        this.setState({
+          // task: [{theTask.activity},{theTask.type},{theTask.particpants}]
+          //task: [theTask.data.activity,theTask.data.type,theTask.data.participants]
+          task: theTask.data
+        });
+      });
+  };
+
+  componentWillReceiveProps() {
+    this.onShowUpdate();
   }
-  render() {                
+
+  onShowUpdate = e => {
+    let status = !this.state.refreshShow;
+    this.setState({ refreshShow: status });
+  };
+
+  render() {
     return (
       <div className="Home">
-        <h2>Home</h2>
-        <p>This is my final project with the MERN stack</p>
-        {this.state.task}<br />
-        <button className="give-task" onClick={this.getActivity}>Get a Random Activity</button>
+        <div className="Add-Task">
+          <AddTask onShowUpdate={this.onShowUpdate} />
+        </div>
+        <div className="generate-task">
+          <h1>Get a Random Activity!</h1>
+          <i>Activity:</i>
+          <h2>{this.state.task.activity}</h2>
+          <br />
+          <i>Type:</i>
+          <h2>{this.state.task.type}</h2>
+          <br />
+          <i>Recommended participants:</i>
+          <h2>{this.state.task.participants}</h2>
+
+          <button className="give-task" onClick={this.getActivity}>
+            Generate
+          </button>
+        </div>
+        <div className="community-tasks">
+          <h2>Community Added Activities</h2>
+          <ShowTasks
+            refreshShow={this.state.refreshShow}
+            user={this.props.user}
+          />
+        </div>
       </div>
     );
   }

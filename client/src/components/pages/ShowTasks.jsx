3aac7324d74352c8fee64fc6448df1111a6fe2e1
Samuel Yamamoto <yamamoto.samuel@gmail.com>
2019-05-24 23:34:14 -0400

    removed unneccessary code and fixed some issues  with passing information through components

--- client/src/components/pages/ShowTasks.jsx
+++ client/src/components/pages/ShowTasks.jsx
@@ -1,43 +1,63 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import api from '../../api'
-
+import React, { Component } from "react";
+import axios from "axios";
+import api from "../../api";
 
 export default class ShowTasks extends Component {
   state = {
-    tasks:[]
+    tasks: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/api/task")
+      .then(tasksFromServer => {
+        console.log("tasksFromServer", tasksFromServer);
+        this.setState({ tasks: tasksFromServer.data });
+      })
+      .catch(err => console.log(err));
   }
 
-  componentDidMount(){
-    axios.get('http://localhost:5000/api/task').then(tasksFromServer=>{
-      console.log('tasksFromServer', tasksFromServer)
-      this.setState({tasks:tasksFromServer.data})
-    }).catch(err=>console.log(err))
+  componentWillReceiveProps() {
+    axios
+      .get("http://localhost:5000/api/task")
+      .then(tasksFromServer => {
+        console.log("tasksFromServer", tasksFromServer);
+        this.setState({ tasks: tasksFromServer.data });
+      })
+      .catch(err => console.log(err));
   }
 
   deleteTask = (id, i) => {
-    console.log(id)//The id in the database
-    //Hero.deleteById(id) 
-    axios.post('http://localhost:5000/api/task/deleteTaskPlease', {id:id}).then(responseFromServer=>{
-      console.log(responseFromServer)
-      let tasks = [...this.state.tasks]
-      tasks.splice(i,1)
-      this.setState({tasks})
-    })
-
-  }
+    console.log(id); //The id in the database
+    //Task.deleteById(id)
+    axios
+      .post("http://localhost:5000/api/task/deleteTaskPlease", { id: id })
+      .then(responseFromServer => {
+        console.log(responseFromServer);
+        let tasks = [...this.state.tasks];
+        tasks.splice(i, 1);
+        this.setState({ tasks });
+      });
+  };
 
   showTheTasks = () => {
-    return this.state.tasks.map((eachTask, i)=>{
-      return <li>{eachTask.activity}<button onClick={()=>this.deleteTask(eachTask._id, i)}>Delete</button></li>
-    })
-  }
+    return this.state.tasks.map((eachTask, i) => {
+      return (
+        <li>
+          {eachTask.activity}
+          {this.props.user? (
+            <button onClick={() => this.deleteTask(eachTask._id, i)}>
+              Delete
+            </button>
+          ) : (
+            ""
+          )}
+        </li>
+      );
+    });
+  };
 
   render() {
-    return (
-      <div>
-        {this.showTheTasks()}
-      </div>
-    )
+    return <div>{this.showTheTasks()}</div>;
   }
 }
